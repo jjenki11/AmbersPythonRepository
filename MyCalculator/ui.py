4097e882c9a1640fe24fdb6bc1bd303e23b02222
jjenki11 <jeff.jenkins.1986@gmail.com>
2016-09-30 13:14:51 -0400

    Added title to window.

--- MyCalculator/ui.py
+++ MyCalculator/ui.py
@@ -1,5 +1,12 @@
+#   Amber's Calculator
+#       ui.py
+
+#   Adding import statements
 import tkinter
 
+#   Construct the window
 window = tkinter.Tk()
-
+#   Set the window's title
+window.title("THIS IS MY WINDOW!")
+#   Run the main loop
 window.mainloop()
